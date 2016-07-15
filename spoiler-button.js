ee8163ac5ffc787d68e46c0373bf2bc9bc3c9f5e
aseel salah <aseel.salah90@yahoo.com>
2016-07-15 13:07:49 +0200

    Create spoiler-button.js

---
+++ spoiler-button.js
@@ -0,0 +1,11 @@
+      
+/*
+Theme Name       : libya web
+Theme site  : https//www.libya-web.blogspot.com
+Theme Designer   : aseel salah
+*/
+$(document).ready(function() {
+$("#spoiler").click(function() {
+$("#spoiler-container").slideToggle("normal");
+ });
+})
